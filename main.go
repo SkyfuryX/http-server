67987a4551be7ba0abb2c551ef8b9af4c73de0d4
SkyfuryX <gannonshanley@gmail.com>
2026-05-18 17:17:26 -0400

    base server setup

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	server := http.Server{
+		Handler: mux,
+		Addr: ":8080",
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		return
+	}
+}
