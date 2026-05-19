980ac590f327c0a509df2de9843799f208831062
SkyfuryX <gannonshanley@gmail.com>
2026-05-19 15:53:19 -0400

    middleware created to handle metrics

--- handlers.go
+++ handlers.go
@@ -1,9 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
-func handlerReady(writer http.ResponseWriter, request *http.Request) {
-	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	writer.WriteHeader(200)
-	writer.Write([]byte("OK"))
+func handlerReady(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
 }
--- main.go
+++ main.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetrics(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
 	server := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
 	}
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", handlerReady)
+	handler := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetrics(handler)))
+	mux.HandleFunc("GET /healthz", handlerReady)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerHits)
+	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
 
 	err := server.ListenAndServe()
 	if err != nil {
