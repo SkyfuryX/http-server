ee2fe35e1d5ccba4b2d15c5fe7773c8b4dfded53
SkyfuryX <gannonshanley@gmail.com>
2026-05-18 20:19:34 -0400

    ready handler set up

--- main.go
+++ main.go
@@ -8,8 +8,11 @@ func main() {
 	mux := http.NewServeMux()
 	server := http.Server{
 		Handler: mux,
-		Addr: ":8080",
+		Addr:    ":8080",
 	}
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handlerReady)
+
 	err := server.ListenAndServe()
 	if err != nil {
 		return
